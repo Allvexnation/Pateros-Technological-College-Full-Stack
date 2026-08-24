7ce6d28e6e6f7278e798ecf4ef3aa02d76873ec5
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-25 06:35:23 +0800

    update the docker

--- backend/src/main/java/com/pateros/backend/BackendApplication.java
+++ backend/src/main/java/com/pateros/backend/BackendApplication.java
@@ -10,15 +10,19 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-			.directory("./")
-			.load();
+		try {
+			Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.load();
 
-		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-		System.setProperty("APIS_ENV", dotenv.get("APIS_ENV"));
+			System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+			System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+			System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+			System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+			System.setProperty("APIS_ENV", dotenv.get("APIS_ENV"));
+		} catch (Exception e) {
+
+		}
 
 		SpringApplication.run(BackendApplication.class, args);
 	}

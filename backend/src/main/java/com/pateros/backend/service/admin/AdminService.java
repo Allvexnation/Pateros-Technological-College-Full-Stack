d2b880c7a01ddf80854cad8bebadc6abd08ebd6d
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-25 13:46:48 +0800

    add the vercel config

--- backend/src/main/java/com/pateros/backend/service/admin/AdminService.java
+++ backend/src/main/java/com/pateros/backend/service/admin/AdminService.java
@@ -2,6 +2,7 @@ package com.pateros.backend.service.admin;
 
 import com.pateros.backend.model.admin.Admin;
 import com.pateros.backend.repository.admin.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +10,10 @@ import java.util.Optional;
 
 @Service
 public class AdminService {
-    @SuppressWarnings("unused")
+    
     private final AdminRepository adminRepository;
     
+    @Autowired
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
     }

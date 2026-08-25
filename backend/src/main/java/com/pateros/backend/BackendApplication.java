59c27ae99c6a5f4998335b367664409d92834bbb
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-25 12:28:59 +0800

    fix the warning

--- backend/src/main/java/com/pateros/backend/BackendApplication.java
+++ backend/src/main/java/com/pateros/backend/BackendApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@SuppressWarnings("unused")
+
 public class BackendApplication {
 
 	public static void main(String[] args) {

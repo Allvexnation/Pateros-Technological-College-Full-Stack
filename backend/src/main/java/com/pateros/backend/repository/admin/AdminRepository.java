8d0fbe74de038fb20336d9dd1f5505708eaf15f9
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-25 12:57:53 +0800

    remove the redundant code

--- backend/src/main/java/com/pateros/backend/repository/admin/AdminRepository.java
+++ backend/src/main/java/com/pateros/backend/repository/admin/AdminRepository.java
@@ -2,11 +2,9 @@ package com.pateros.backend.repository.admin;
 
 import com.pateros.backend.model.admin.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
     Optional<Admin> findByEmail(String email);
     Optional<Admin> findByUsername(String username);
--- backend/src/main/java/com/pateros/backend/service/admin/AdminService.java
+++ backend/src/main/java/com/pateros/backend/service/admin/AdminService.java
@@ -2,7 +2,6 @@ package com.pateros.backend.service.admin;
 
 import com.pateros.backend.model.admin.Admin;
 import com.pateros.backend.repository.admin.AdminRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +9,9 @@ import java.util.Optional;
 
 @Service
 public class AdminService {
-    
+    @SuppressWarnings("unused")
     private final AdminRepository adminRepository;
     
-    @Autowired
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
     }
